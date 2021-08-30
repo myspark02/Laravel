ad3bebb71bff11e79f60448ccb44d7d7b7e8f705
myspark02 <myspark02@gmail.com>
2021-08-31 00:54:40 +0900

    livewire

---
+++ livewire/resources/views/livewire/counter.blade.php
@@ -0,0 +1,9 @@
+<div style="text-align: center">
+    {{-- Do your work, then step back. --}}
+    <div>
+        I am a couter component
+    </div>
+    <button wire:click="increment">+</button>
+    <h1>{{ $count }} </h1>
+    <button wire:click="decrement">-</button>
+</div>
---
+++ livewire/resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Livewire</title>
+
+    @livewireStyles
+</head>
+
+<body>
+    @livewire('counter')
+    @livewireScripts
+</body>
+
+</html>
