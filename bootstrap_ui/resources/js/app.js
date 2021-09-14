9552453257a1eed15f4c862c9dba9a93d5e30b74
myspark02 <myspark02@gmail.com>
2021-09-14 18:46:19 +0900

    Todo list implementd. vue&Laravel

--- bootstrap_ui/resources/js/app.js
+++ bootstrap_ui/resources/js/app.js
@@ -1,1 +1,24 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
 require('./bootstrap');
+
+import Vue from 'vue';
+import App from './vue/app';
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPlusSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faPlusSquare, faTrash)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+
+const app = new Vue( {
+    el : '#app',
+    components: {App}
+});
--- bootstrap_ui/webpack.mix.js
+++ bootstrap_ui/webpack.mix.js
@@ -11,8 +11,12 @@ const mix = require('laravel-mix');
  |
  */
 
+
 mix.js('resources/js/app.js', 'public/js').vue()
-    .sass('resources/sass/app.scss', 'public/css')
     .postCss('resources/css/app.css', 'public/css', [
-        require('tailwindcss'),])
-    .sourceMaps();
+        require('tailwindcss'),
+    ]);
+
+if (mix.inProduction()) {
+    mix.version();
+}
