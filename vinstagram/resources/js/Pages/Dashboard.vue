a78dc0f1b8cff1859e22a23b43ce1266420110bd
myspark02 <myspark02@gmail.com>
2021-09-27 21:05:59 +0900

    initial commit

---
+++ vinstagram/resources/js/Pages/Dashboard.vue
@@ -0,0 +1,33 @@
+<template>
+    <app-layout title="Dashboard">
+        <template #header>
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">
+                {{ username }}
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div class="overflow-hidden bg-white shadow-xl sm:rounded-lg">
+                    <post-list />
+                </div>
+            </div>
+        </div>
+    </app-layout>
+</template>
+
+<script>
+    import { defineComponent } from 'vue'
+    import AppLayout from '@/Layouts/AppLayout.vue'
+    import PostList from '@/Pages/Instagram/PostList.vue'
+
+    export default defineComponent({
+        props: {
+            username: String,
+        },
+        components: {
+            AppLayout,
+            PostList,
+        },
+    })
+</script>
---
+++ vinstagram/resources/js/Pages/Instagram/PostList.vue
@@ -0,0 +1,21 @@
+<template>
+
+  <div class="grid grid-cols-1 gap-5 p-10 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3">
+    <!--Card 1-->
+    <post-item v-for="i in posts" :key="i"/>
+  </div>
+
+</template>
+
+<script>
+import PostItem from '@/Pages/Instagram/PostItem.vue';
+export default {
+    components: {PostItem},
+    data() {
+        return {
+            posts :[1, 2, 3, 4, 5, 6, 7],
+        };
+    }
+
+}
+</script>
