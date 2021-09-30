8760ba7c17c88fb01dda8ecfbe7defc2fb058d39
myspark02 <myspark02@gmail.com>
2021-10-01 00:20:33 +0900

    Cache used to store  user,  followers count etc

--- vinstagram/app/Http/Controllers/ProfilesController.php
+++ vinstagram/app/Http/Controllers/ProfilesController.php
@@ -6,37 +6,54 @@ use App\Models\Profile;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
 use Inertia\Inertia;
 
 class ProfilesController extends Controller
 {
     public function index($name)
     {
+        $user = Cache::remember(
+            'user.' . $name,
+            now()->addSeconds(30),
+            function () use ($name) {
+                return User::where('name', $name)->first();
+            }
+        );
 
-        $user = User::where('name', $name)->first();
+        if ($user) {
+            $posts = Cache::remember(
+                'count.posts.' . $user->id,
+                now()->addSeconds(30),
+                function () use ($user) {
+                    return $user->posts;
+                }
+            );
 
-        if ($user)
+            $followers = Cache::remember(
+                'count.followers.' . $user->id,
+                now()->addSeconds(30),
+                function () use ($user) {
+                    return $user->profile->followers->count();
+                }
+            );
             return Inertia::render('Dashboard', [
                 'user' => Auth::user(),
-                'posts' => $user->posts,
+                'posts' => $posts,
                 'can' => ['create_update' => Auth::user()->id == $user->id],
                 'viewed_user' => $user,
-                'followers' => $user->profile->followers->count(),
+                'followers' => $followers,
             ]);
-        else
+        } else
             return Inertia::render('Notfound');
     }
 
     public function update(Request $request)
     {
-
-
+        // 본인 프로파일 업데이트
         $data = $request->validate(['title' => 'required', 'description' => 'required',]);
-
         Auth::user()->profile->update($data);
-
         // dd($request->all());
-
         return Inertia::render(
             'Dashboard',
             [
@@ -44,7 +61,7 @@ class ProfilesController extends Controller
                 'posts' => fn () => Auth::user()->posts,
                 'can' => ['create_update' => true],
                 'viewed_user' => Auth::user(),
-                'followers' => $user->profile->followers->count(),
+                'followers' => Auth::user()->profile->followers->count(),
             ]
         );
     }
